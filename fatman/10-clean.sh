3929d6d101af2b43ec02496a7ba19cd4b891e313
Rikard Bartholf <rikard@bartholf.nu>
2015-07-26 22:32:31 +0200

    Restructured things

---
+++ fatman/10-clean.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+loadkeys sv-latin1
+
+echo "Initializing disks"
+mkfs.vfat -F32 -I /dev/sda1
+
+echo "Formatting /"
+mkfs.ext4 /dev/sda2
+
+echo "Mount disks"
+mount /dev/sda2 /mnt
+mkdir -p /mnt/{home,var,boot}
+mkdir -p /mnt/opt/storage
+mount /dev/sda1 /mnt/boot
+mount /dev/sdc1 /mnt/home
+mount /dev/sdb1 /mnt/opt/storage
+
+echo "Server = http://ftp.lysator.liu.se/pub/archlinux/\$repo/os/\$arch
+Server = http://ftp.availo.se/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+
+pacstrap /mnt base base-devel
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+nano /mnt/etc/fstab
+nano /mnt/etc/makepkg.conf
+
+mkdir -p /mnt/install
+
+cp ./20-chroot.sh /mnt/install
+arch-chroot /mnt /install/20-chroot.sh
+
+mkdir -p /mnt/etc/skel/{Downloads,Documents,Pictures}
+
+umount /mnt/{home,boot,}
---
+++ fatman/40-XFCE4.12.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+echo "[archlinuxfr]
+# The French Arch Linux communities packages.
+SigLevel = Never
+Server = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf
+
+pacman -Syu
+pacman -S --noconfirm yaourt
+pacman -S --noconfirm nvidia pangox-compat
+pacman -S --noconfirm xf86-video-intel
+pacman -S --noconfirm xorg-server xorg-xinit xorg-server-utils
+pacman -S --noconfirm xfce4 xfce4-goodies
+pacman -S --noconfirm sudo ttf-dejavu networkmanager network-manager-applet
+pacman -S --noconfirm gvfs gvfs-afp gvfs-gphoto2 gvfs-afc gvfs-goa gvfs-mtp gvfs-smb
+pacman -S --noconfirm polkit-gnome gnome-keyring
+pacman -S --noconfirm pulseaudio pavucontrol
+pacman -S --noconfirm gst-libav gst-plugins-good gst-plugins-bad gst-plugins-ugly
+pacman -S --noconfirm virtualbox virtualbox-host-modules virtualbox-host-dkms
+pacman -S --noconfirm ttf-inconsolata ttf-ubuntu-font-family ttf-droid
+pacman -R --noconfirm mousepad
+pacman -S --noconfirm gedit
+pacman -S --noconfirm firefox flashplugin
+
+systemctl enable NetworkManager.service
+systemctl enable dkms.service
+
+useradd -m -G wheel,vboxusers,storage,lp,power -s /bin/bash rib
+echo "Password for rib"
+passwd rib
+
+visudo
+
+echo "vboxdrv
+vboxnetflt" > /etc/modules-load.d/virtualbox.conf
+
+#pacman -S --noconfirm gstreamer0.10-good-plugins gstreamer0.10-bad-plugins gstreamer0.10-ugly-plugins gstreamer0.10-ffmpeg
+#yaourt -S --noconfirm xfce-theme-greybird-git adduser
+#yaourt -S --noconfirm xfce4-volumed-pulse
